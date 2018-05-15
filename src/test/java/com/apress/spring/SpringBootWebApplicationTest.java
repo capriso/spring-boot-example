a93f2dfa293e2361983fc1e18c279b6cf349d191
Teddy <capriso@gmail.com>
2018-05-16 01:34:22 +0900

    add junit test for spring boot

---
+++ src/test/java/com/apress/spring/SpringBootWebApplicationTest.java
@@ -0,0 +1,91 @@
+package com.apress.spring;
+
+import com.apress.spring.domain.JournalEntry;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.http.MockHttpOutputMessage;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class SpringBootWebApplicationTest {
+    private final String SPRING_BOOT_MATCH = "Spring Boot";
+    private final String CLOUD_MATCH = "Cloud";
+
+    @SuppressWarnings("rawtypes")
+    private HttpMessageConverter mappingJackson2HttpMessageConverter;
+    private MediaType contentType = new MediaType(
+            MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    void setConverters(HttpMessageConverter<?>[] converters) {
+        this.mappingJackson2HttpMessageConverter = Arrays.stream(converters)
+                .filter(converter -> converter instanceof MappingJackson2HttpMessageConverter)
+                .findAny().orElse(null);
+    }
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/journal/all"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(contentType))
+                .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.iterableWithSize(4)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0]['title']", Matchers.containsString(SPRING_BOOT_MATCH)));
+    }
+
+    @Test
+    public void testFindByTitle() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/journal/findBy/title/" + CLOUD_MATCH))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(contentType))
+                .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.iterableWithSize(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0]['title']", Matchers.containsString(CLOUD_MATCH)));
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/journal")
+                        .content(this.toJsonString(
+                                new JournalEntry("Spring Boot Test", "Create Spring Boot Test", "01/01/2018")))
+                .contentType(contentType))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @SuppressWarnings("unchecked")
+    private String toJsonString(Object obj) throws IOException {
+        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
+        this.mappingJackson2HttpMessageConverter.write(obj, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
+        return mockHttpOutputMessage.getBodyAsString();
+    }
+}
